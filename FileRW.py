1024da0449d125fb9bed047a6bc422d9c69ccd60
최재용 <jayong93@gmail.com>
2017-09-20 21:41:47 +0900

    unix 시간도 저장하게 변경 및 변수 이름 변경

--- FileRW.py
+++ FileRW.py
@@ -6,54 +6,53 @@ class FileRW:
     def __init__(self):
         self.data_file = None
         self.log_file = None
-        self.name = None
+        self.account = None
 
-    def is_exist(self, name):
-        self.name = name
-        data_path = os.path.join(os.getcwd(), name + '.data')
-        log_path = os.path.join(os.getcwd(), name + '.log')
+    def is_exist(self, account):
+        self.account = account
+        data_path = os.path.join(os.getcwd(), account + '.data')
+        log_path = os.path.join(os.getcwd(), account + '.log')
         if not (os.path.exists(data_path) and os.path.exists(log_path)):
             self.data_file = open(data_path, 'w+', encoding='utf-8')
             self.log_file = open(log_path, 'w+', encoding='utf-8')
-            return (False, None)
+            return (False, None, None)
 
-        last_time = self.sync_between_data_log(data_path, log_path)
-        if last_time:
-            return (True, last_time)
+        (last_id, last_time) = self.sync_between_data_log(data_path, log_path)
+        if last_id and last_time:
+            return (True, last_id, last_time)
         else:
-            return (False, None)
+            return (False, None, None)
 
     def sync_between_data_log(self, data_path, log_path):
         self.log_file = open(log_path, 'r+', encoding='utf-8')
         if self.log_file:
-            log_time_list = self.log_file.read().split('\n')
-            if log_time_list:
-                data_pattern = re.compile(r'(^|\n\n)?(\d+)(\n<p.*>.+</p>)')
+            log_id_list = self.log_file.read().split('\n')
+            if log_id_list:
+                data_pattern = re.compile(r'(^|\n\n)?(\d+)(\n)(\d+)(\n)(<p.*>.+</p>)')
 
                 self.data_file = open(data_path, 'r+', encoding='utf-8')
-                if log_time_list[-1]:
-                    last_log_time = log_time_list[-1]
+                if log_id_list[-1]:
+                    last_log_id = log_id_list[-1]
                 else:
-                    last_log_time = log_time_list[-2]
+                    last_log_id = log_id_list[-2]
                 data_list = data_pattern.split(self.data_file.read())
 
-                idx = data_list.index(last_log_time)
+                idx = data_list.index(last_log_id)
                 self.data_file.seek(0)
                 self.data_file.truncate()
-                self.data_file.write(''.join(data_list[:idx + 2]))
+                self.data_file.write(''.join(data_list[:idx + 5]))
 
-                return int(last_log_time)
+                return (int(last_log_id), int(data_list[idx+2]))
             else:
                 self.data_file = open(data_path, 'w+', encoding='utf-8')
-                return None
+                return (None, None)
 
     def write_tweet_list(self, tw_list):
-        for id, data in reversed(tw_list):
-            self.write_tweet(id, data)
+        for id, time, data in reversed(tw_list):
+            self.write_tweet(id, time, data)
 
-    def write_tweet(self, time, data):
-        data_time = str(time)
-        full_data = data_time + '\n' + data
+    def write_tweet(self, id, time, data):
+        full_data = str(id) + '\n' + str(time) + '\n' + data
 
         try:
             if self.data_file.tell() == 0:
@@ -62,9 +61,9 @@ class FileRW:
                 self.data_file.write('\n\n' + full_data)
 
             if self.log_file.tell() == 0:
-                self.log_file.write(data_time)
+                self.log_file.write(str(time))
             else:
-                self.log_file.write('\n' + data_time)
+                self.log_file.write('\n' + str(time))
         except:
             print('write err')
 

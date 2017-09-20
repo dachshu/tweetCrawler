84febd413b03731fc642e2157fe881d981cb82fb
Jae-Yong Choi <jayong93@gmail.com>
2017-09-20 14:32:32 +0900

    main 코드 순서 변경 FileRW 오류 수정

--- FileRW.py
+++ FileRW.py
@@ -15,37 +15,41 @@ class FileRW:
         if not (os.path.exists(data_path) and os.path.exists(log_path)):
             self.data_file = open(data_path, 'w+', encoding='utf-8')
             self.log_file = open(log_path, 'w+', encoding='utf-8')
-            return False
+            return (False, None)
 
-        last_time = self.SyncBetweenDataLog(data_path, log_path)
+        last_time = self.sync_between_data_log(data_path, log_path)
         if last_time:
             return (True, last_time)
         else:
-            return False
+            return (False, None)
 
     def sync_between_data_log(self, data_path, log_path):
         self.log_file = open(log_path, 'r+', encoding='utf-8')
         if self.log_file:
-            log_time_list = self.log_file.readlines()
+            log_time_list = self.log_file.read().split('\n')
             if log_time_list:
-                data_pattern = re.compile(r'(^|\n\n)?(\d+)(\n<p>.+</p>)')
+                data_pattern = re.compile(r'(^|\n\n)?(\d+)(\n<p.*>.+</p>)')
 
                 self.data_file = open(data_path, 'r+', encoding='utf-8')
-                last_log_time = log_time_list[-1]
+                if log_time_list[-1]:
+                    last_log_time = log_time_list[-1]
+                else:
+                    last_log_time = log_time_list[-2]
                 data_list = data_pattern.split(self.data_file.read())
 
                 idx = data_list.index(last_log_time)
+                print(idx)
                 self.data_file.seek(0)
                 self.data_file.truncate()
                 self.data_file.write(''.join(data_list[:idx + 2]))
 
-                return float(last_log_time)
+                return int(last_log_time)
             else:
                 self.data_file = open(data_path, 'w+', encoding='utf-8')
                 return None
 
     def write_tweet_list(self, tw_list):
-        for id, data in tw_list:
+        for id, data in reversed(tw_list):
             self.write_tweet(id, data)
 
     def write_tweet(self, time, data):
@@ -66,13 +70,6 @@ class FileRW:
             print('write err')
 
 
-if __name__ == "__main__":
-    frw = FileRW()
-
-    frw.IsExist('test')
-    #frw.WriteTweet(100, '<p>test string1</p>')
-    #frw.WriteTweet(200, '<p>Test String2</p>')
-    frw.WriteTweet(300, '<p>test String3</p>')
-    frw.WriteTweet(400, '<p>Test string4</p>')
-
-    print('end')
+if __name__ == '__main__':
+    with open('realDonaldTrump.log', 'r+t') as f:
+        print(f.read().split('\n'))
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import TweetCrawler
 import FileRW
 
+account = input('account: ')
+
 file_manager = FileRW.FileRW()
 tweetCrawler = TweetCrawler.TweetCrawelr()
 
-account = input('account: ')
 exist, last_id = file_manager.is_exist(account)
 tweets = tweetCrawler.crawl(account, exist, last_id)
 file_manager.write_tweet_list(tweets)

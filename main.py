1adcbf6779c402eb2e63e4b446b187403338b425
Jae-Yong Choi <jayong93@gmail.com>
2017-10-11 14:01:45 +0900

    계정 크롤링을 반복하도록 수정

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ tweetCrawler = TweetCrawler.TweetCrawelr()
 
 tweetCrawler.update()
 
-a = input("트윗 크롤링 실행? ")
-if( a =='y' or a == 'yes'):
-    account = input('account: ')
-    join_year = input('join year: ')
-    join_month = input('join month: ')
-    tweetCrawler.crawling(account, int(join_year), int(join_month))
+while True:
+    a = input("트윗 크롤링 실행? ")
+    if( a =='y' or a == 'yes'):
+        account = input('account: ')
+        join_year = input('join year: ')
+        join_month = input('join month: ')
+        tweetCrawler.crawling(account, int(join_year), int(join_month))
+    else: break
